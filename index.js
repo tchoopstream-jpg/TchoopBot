24be9777b4428d2e2aa2d04d7b2d47776d05b475
Tchoop <tchoop.stream@gmail.com>
2026-05-28 21:46:45 +0200

    fix intents

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+
   ]
 });
 
