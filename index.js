69ee5864749911c2754e3ad0935d171676e81927
Tchoop <tchoop.stream@gmail.com>
2026-05-28 21:26:41 +0200

    Premier bot

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Bot connecté : ${client.user.tag}`);
+});
+
+client.on("guildMemberAdd", member => {
+  const channel = member.guild.channels.cache.find(
+    c => c.name === "👋・bienvenue"
+  );
+
+  if (!channel) return;
+
+  channel.send(
+    `👋 Bienvenue ${member} sur le serveur officiel de Tchoop !`
+  );
+});
+
+client.login(process.env.TOKEN);
